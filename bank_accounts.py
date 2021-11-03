9cb457b7ccd39da2ab93463a904038d094a61b6b
Nathaniel Ramirez <nramire01@gmail.com>
2021-11-03 05:27:52 -0500

    update

--- bank_accounts.py
+++ bank_accounts.py
@@ -1,12 +1,18 @@
 class BankAccount:
+    accounts = []
     def __init__(self, balance=0, int_rate=0.015):
         self.balance = balance
         self.int_rate = int_rate
+        BankAccount.accounts.append(self)
     def deposit(self,amount):
         self.balance +=amount
         return self
-    def withdrawal(self,amount):
-        self.balance -=amount
+    def withdrawal(self,amount): #forgot to add this until looking at solution
+        if(self.balance-amount) >= 0:
+            self.balance -=amount
+        else:
+            print("Insufficient Funds: Charging $5 Overdraft Fee")
+            self.balance -= 5
         return self
     def display_account_info(self):
         print(f"Balance: {self.balance}")
@@ -15,9 +21,12 @@ class BankAccount:
         if self.balance > 0:
             self.balance = self.balance + (self.int_rate * self.balance)
             return self
-    @classmethod
-        
+    @classmethod #only added this after looking into solution
+    def display_all_accounts(cls):
+            for account in cls.accounts:
+                account.display_account_info()
 account_1 = BankAccount(200)
 account_2 = BankAccount(900)
 account_1.deposit(200).deposit(900).deposit(300).withdrawal(200).yield_interest().display_account_info()
 account_2.deposit(50).deposit(50).withdrawal(250).withdrawal(250).withdrawal(250).withdrawal(249).yield_interest().display_account_info()
+BankAccount.display_all_accounts()
